48be9eab335bc4000f11fe16613ffaff65e78da0
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-02-15 11:43:45 +0530

    Create Chef and Brain Speed.c

---
+++ Chef and Brain Speed.c
@@ -0,0 +1,16 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	  int X, Y; 
+    scanf("%d%d",&X, &Y); 
+    if(Y>X) {
+    printf("YES\n");
+    }
+    else{
+    printf("NO\n");
+    }
+	return 0;
+}
+
