d44bf935479af2e720de5e12d822c22fb8869fca
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-02-18 09:22:50 +0530

    Create Counting Words.c

---
+++ Counting Words.c
@@ -0,0 +1,14 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+printf("\n%d",a*b); }
+	// your code goes here
+	return 0;
+}
+
