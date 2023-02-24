2e2d8a7e31fa620568c3312fd9caab788e78edf1
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-02-24 08:48:28 +0530

    Create Audible Range.c

---
+++ Audible Range.c
@@ -0,0 +1,24 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+    int t;
+scanf("%d",&t);
+while(t--)
+{
+    int x;
+    scanf("%d",&x);
+    if(x>=67&&x<=45000)
+    {
+        printf("YES \n");
+    }
+    else
+    {
+        printf("NO \n");
+    }
+    
+    
+}
+	return 0;
+}
+
