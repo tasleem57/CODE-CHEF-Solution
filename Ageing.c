7250d7bae57fa14bc30d5ced5e7e0805aba9b38d
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-03 09:15:11 +0530

    Create Ageing.c

---
+++ Ageing.c
@@ -0,0 +1,15 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n;
+	    scanf("%d",&n);
+	    
+	    printf("%d\n",abs(10-n));
+	}
+	return 0;
+}
