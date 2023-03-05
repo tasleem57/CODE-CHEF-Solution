3d6fb1ba84ca259c46dedb059a83fc55837fcc7e
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-05 08:36:48 +0530

    Create Good Turn.c

---
+++ Good Turn.c
@@ -0,0 +1,17 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if (x+y<=6) 
+	    printf("NO\n");
+	    else printf("YES\n");
+	    
+	}
+	return 0;
+}
