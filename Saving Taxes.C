8feb93684381b683fda20b8344e81e4a556e57b8
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-07 09:03:50 +0530

    Create Saving Taxes.C

---
+++ Saving Taxes.C
@@ -0,0 +1,15 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--) {
+        int a,b,n;
+        scanf("%d %d",&a,&b);
+        n=a-b;
+        printf("%d\n",n);
+    }
+	return 0;
+}
+
