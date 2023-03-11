9a035eb612e3968d571ba1b50cae525bcfa53a33
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-11 11:42:34 +0530

    Create Squats.C

---
+++ Squats.C
@@ -0,0 +1,15 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    printf("%d\n",x*15);
+	}
+	return 0;
+}
+
