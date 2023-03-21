0e723ab7a74be96506c81c81138cb73ca8d4d4c4
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-21 20:18:52 +0530

    Create Get Subscription.C

---
+++ Get Subscription.C
@@ -0,0 +1,20 @@
+/* TASLEEM BANU S */
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d", &t);
+	for(int i=1;i<=t;i++){
+	    int x;
+	    scanf("%d", &x);
+	    if(x >30){
+	        printf("Yes\n");
+	    }
+	    else{
+	        printf("No\n");
+	    }
+	}
+	return 0;
+}
+
