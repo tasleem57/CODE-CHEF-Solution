d3dc55b30a9c02b602ed7108a971fe81fb5a8eee
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-22 18:09:39 +0530

    Create Podium Finish.c

---
+++ Podium Finish.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int t;
+scanf ("%d",&t);
+while(t--)
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+}
+	return 0;
+}
+
