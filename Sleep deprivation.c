9adf0df52b75eb0c0b526026db77aa6729fd8a10
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-29 08:25:42 +0530

    Create Sleep deprivation.c

---
+++ Sleep deprivation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int t;
+  scanf("%d",&t);
+  while(t--){
+    int x;
+    scanf("%d",&x);
+    if(x<7)
+    printf("yes\n");
+   else
+    printf("no\n");
+  }
+	// your code goes here
+	return 0;
+}
+
