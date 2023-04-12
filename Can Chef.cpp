e97660112876866e89151c1101c37c79d22db800
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-04-12 22:41:28 +0530

    Create Can Chef.cpp

---
+++ Can Chef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int z;
+	cin>>z;
+	while(z--){
+	    int f,g;
+	    cin>>f>>g;
+	    if(f*15>=2*g)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
